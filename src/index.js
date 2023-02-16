4c7b939a8162d1746e2251d1b0394527b141cabf
Oleksii-Kryventsov <kryventsov@gmail.com>
2023-02-16 21:19:14 +0200

    copy team project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./js/slider";
+import "./js/mobile-menu";
+import "./js/read-more";
+import "./js/about-product";
+import "./js/franchise-modal";
+import "./js/our-location";
+import "./js/buy-now";
